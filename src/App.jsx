a410bf637386eae6663f60440416539b1e80e210
MANISH R <manishrajaa2005@gmail.com>
2025-09-08 10:39:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import { Outlet } from 'react-router-dom'
 import { useDispatch } from "react-redux";
 import { login, logout } from "./store/authSlice";
